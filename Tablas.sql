2c87f7e4065bc6b21fa3ac661a393c43ed78f66e
Javier Alejandro Rincón <110916364+RainRevan@users.noreply.github.com>
2022-09-22 14:08:26 -0500

    base de datos
    Base de datos añadida al proyecto - Desarrollo de David Guerrero

---
+++ Tablas.sql
@@ -0,0 +1,13 @@
+-- Database: Proyecto
+
+-- DROP DATABASE IF EXISTS "Proyecto";
+
+CREATE DATABASE "Proyecto"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Colombia.1252'
+    LC_CTYPE = 'Spanish_Colombia.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
